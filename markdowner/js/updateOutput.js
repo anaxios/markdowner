6b00ef0bf11f274e5a2330c3875305538f49ec32
Bobby Sanguinetti <anaxios@daedalist.net>
2017-12-21 23:54:25 -0800

    update

--- markdowner/js/updateOutput.js
+++ markdowner/js/updateOutput.js
@@ -1,6 +1,6 @@
 function updateOutput() {
     var converter = new showdown.Converter();
     $("#htmlPanel").html(converter.makeHtml($("#mdPanel").val()));
-    $("iframe").contents().find("head").html("<style type='text/css'>" + $("#cssPanel").val() + "</style>");
+    $("iframe").contents().find("head").html("<style type='text/css'>" + $("#cssPanel").val() + "</style> <base target='_blank'>");
     $("iframe").contents().find("body").html($("#htmlPanel").val());
 }
