85dbad6fc7703264e3ef7d260afc0a6f68e50ac7
anaxios <anaxios@daedalist.net>
2017-12-29 23:05:39 -0800

    update

--- markdowner/js/parser.js
+++ markdowner/js/parser.js
@@ -1,3 +1,45 @@
-<script src="//cdn.jsdelivr.net/npm/ramda@0.25/dist/ramda.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/immutable/3.8.2/immutable.min.js"></script>
+var lexicon = {
+    T_HEAD: "<h1>",
+    T_HEAD_END: "</h1>",
+};
+
+var htmlOutput = [];
+
+var markDowner = (md) => {
+// get input as string
+
+// look through string for markdown
+for (c of md) {
+    mdParser(c);
+}
+return htmlOutput;
+// add each element to parser list
+
+// transfor lexicon to html tags
+}
+
+var mdParser = c => {
+    if (c === "#") {
+        htmlOutput.unshift("T_HEAD");
+    } else if (c == "\n") {
+        if (backTrace(c) == true) htmlOutput.unshift("T_HEAD_END");
+    } else {
+        htmlOutput.unshift(c);
+    }
+    return htmlOutput;
+};
+
+var backTrace = c => {
+    for (element of htmlOutput){
+     if (element === "T_HEAD") {break; return true}
+     else {return false}
+    } 
+   // else if (c.find(isExcapeChar)) {}
+   
+    
+}
+
+var isT_HEAD = c => {
+    return c === "T_HEAD";
+}
 
