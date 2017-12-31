70249dec5fcfe47c7a2b9a99703deb77da119fdc
Bobby Sanguinetti <anaxios@daedalist.net>
2017-12-31 13:34:22 -0800

    t_head tags work mostly

--- markdowner/js/parser.js
+++ markdowner/js/parser.js
@@ -2,44 +2,57 @@ var lexicon = {
     T_HEAD: "<h1>",
     T_HEAD_END: "</h1>",
 };
-
+var T_HEAD = ["T_HEAD_1", "T_HEAD_2", "T_HEAD_3", "T_HEAD_4", "T_HEAD_5", "T_HEAD_6", ]
 var htmlOutput = [];
 
-var markDowner = (md) => {
-// get input as string
+var markDowner = (inputText) => {
+    let hashAccum = 0;
+    for (let i = 0; inputText.length > i; i++) {  
+        if (inputText.charAt(i) && inputText.charAt((i + 1)) === "#") {
+            hashAccum++;
+        } else if (hashAccum > 0 && hashAccum < 6 && inputText.charAt(i + 1) != "#") {
+            htmlOutput.unshift(T_HEAD[hashAccum - 1]);
+            hashAccum = 0;
+        } else if (inputText.charAt(i) == "#" && inputText.charAt(i + 1) != "#") {
+            htmlOutput.unshift(T_HEAD[hashAccum]);
+        } else if (hashAccum > 5) {
+            while (hashAccum > 0) {htmlOutput.unshift("#"); hashAccum--;}
+        } else {
+            htmlOutput.unshift(inputText.charAt(i));
+        }
 
-// look through string for markdown
-for (c of md) {
-    mdParser(c);
-}
-return htmlOutput;
-// add each element to parser list
-
-// transfor lexicon to html tags
-}
-
-var mdParser = c => {
-    if (c === "#") {
-        htmlOutput.unshift("T_HEAD");
-    } else if (c == "\n") {
-        if (backTrace(c) == true) htmlOutput.unshift("T_HEAD_END");
-    } else {
-        htmlOutput.unshift(c);
+        
     }
     return htmlOutput;
-};
 
-var backTrace = c => {
+}
+
+var mdParser = character => {
+    if (character === "#") {
+        return "T_HEAD";
+    } else if (character === "\n") {
+        if (backTrace() == true) return "T_HEAD_END"; // 
+    } else {
+        return character;
+    }
+}
+
+var backTrace = character => {
     for (element of htmlOutput){
-     if (element === "T_HEAD") {break; return true}
-     else {return false}
+        switch (element) {
+            case "T_HEAD_END":
+                return false;
+                break;
+            case "T_HEAD":
+                return true;
+                break;
+            default:
+                continue;
+        }
     } 
    // else if (c.find(isExcapeChar)) {}
    
     
 }
 
-var isT_HEAD = c => {
-    return c === "T_HEAD";
-}
 
